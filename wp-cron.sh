835d63905257601a8b2a64b5e3c62725dc5173b3
Robert Daunce <rdaunce@users.noreply.github.com>
2018-03-14 15:26:53 -0400

    Create wp-cron.sh
    Initial commit

---
+++ wp-cron.sh
@@ -0,0 +1,195 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+
+init () {
+        printf "Locating WordPress installations"
+
+        while read line
+        do
+                let i++
+
+                configcheck="false"
+
+                wp config has DISABLE_WP_CRON --path=$line
+
+
+                if [ $? -eq 0 ]
+                then
+                        configcheck=`wp config get DISABLE_WP_CRON --path=$line`
+                fi
+
+                wp_cron[$i]=[$configcheck]
+
+                INSTPATH[$i]=$line
+
+                printf "."
+        done <<< "$(find | grep wp-config.php | sed -r 's:/wp-config.php::' | sort )"
+
+        echo ""
+        echo Found $i WordPress installations
+}
+
+printmenu(){
+        echo "Main Menu"
+        echo ""
+        echo "1:  Set DISABLE_WP_CRON to true on all WordPress Installations"
+        echo "2:  Set DISABLE_WP_CRON to false on all WordPress Installations"
+        echo "3:  List all WordPress Installations"
+        echo "4:  Toggle DISABLE_WP_CRON on a single WordPress Installation"
+        echo "5:  Run all WP Cron jobs due"
+        echo "6:  Exit"
+        echo ""
+        echo "Select Item"
+}
+
+printlist(){
+        for ((index=1;index<=$i;index++))
+        do
+                if [ ${wp_cron[$index]} = "[true]" ]
+                then
+                        color=$GREEN
+                else
+                        color=$RED
+                fi
+
+                echo -e "${index} : ${CYAN}${INSTPATH[$index]}${NC} : DISABLE_WP_CRON = ${color}${wp_cron[$index]}${NC}"
+        done
+}
+
+toggle(){
+        path="${INSTPATH[$1]}"
+        if [ ${wp_cron[$1]} = "[true]" ]
+        then
+                newvalue="false"
+        else
+                newvalue="true"
+        fi
+
+        result=`wp config set DISABLE_WP_CRON $newvalue --type=constant --path=${INSTPATH[$1]}`
+        if [ $? -ne 0 ]
+        then
+                echo "Failed to set DISABLE_WP_CRON to $newvalue for ${INSTPATH[$1]}"
+        else
+                configcheck=`wp config get DISABLE_WP_CRON --path=${INSTPATH[$1]}`
+                wp_cron[$1]=[$configcheck]
+        fi
+
+}
+
+setall(){
+        for ((index=1;index<=$i;index++))
+        do
+                result=`wp config set DISABLE_WP_CRON $1 --type=constant --path=${INSTPATH[$index]}`
+
+                if [ $? -ne 0 ]
+                then
+                        echo "Failed to set DISABLE_WP_CRON to $1 for ${INSTPATH[$index]}"
+                fi
+
+                wp_cron[$i]=[$configcheck]
+        done
+}
+
+wp-cron () {
+        echo "**************** Starting wp-cron **************"
+        echo `date -u  +'%F %T %:z'` - Running wp-cron
+        echo
+
+        for ((index=1;index<=$i;index++))
+        do
+                if [ ${wp_cron[$index]} = "[true]" ]
+                then
+                        if [ "$interactive" = "1" ]; then
+                                echo -e "${CYAN}`date -u  +'%F %T %:z'` - ${INSTPATH[$index]}${NC}"
+                        else
+                                echo -e "`date -u  +'%F %T %:z'` - ${INSTPATH[$index]}"
+                        fi
+
+                        wp cron event run --due-now --path=${INSTPATH[$index]}
+                fi
+        done
+        echo
+        echo "**************** wp-cron complete  **************"
+}
+
+usage(){
+        echo "help"
+        echo ""
+}
+
+declare -a INSTPATH
+i=0
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -r | --runall )         init
+                                wp-cron
+                                exit
+                                ;;
+        -i | --interactive )    init
+                                interactive=1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+if [ "$interactive" = "1" ]; then
+while true; do
+                printmenu
+
+                read menuid
+                echo ""
+
+                case $menuid in
+                1)
+                        setall "true"
+                        ;;
+                2)
+                        setall "false"
+                        ;;
+                3)
+                        printlist
+                        ;;
+                4)
+                        let installid=-1
+                        let invalid=0
+
+                        while [ "$installid" -lt "0" ] || [ "$installid" -gt "$i" ]; do
+                                if [ "$invalid" -eq "1" ]
+                                then
+                                        echo "Invalid choice"
+                                fi
+                                echo -n "What WordPress Installation would you like to toggle?  Enter the number from the list (0 to cancel):"
+
+                                read  installid
+                                let invalid=1
+                        done
+                        echo ""
+                        toggle $installid
+                        ;;
+                5)
+                        wp-cron
+                        ;;
+                6)
+                        exit
+                        ;;
+                *)
+                        echo "Invalid choice"
+                        ;;
+                esac
+        done
+else
+        usage
+fi
+
+
+
